dd44c6a91a99293f1f8d7852b8e25b7c49bd27e4
Denis Alicic <a.denis96@gmail.com>
2019-05-20 11:21:08 +0200

    Added support for more faces

--- demo/predict.py
+++ demo/predict.py
@@ -31,13 +31,13 @@ def main():
             }
     args = parser.parse_args()
     img = cv2.imread(args.input_path)
-    #frame = img[:, :, ::-1]
-    #face_embedding = np.array(face_recognition.face_encodings(frame))
     face_embedding = np.array(face_recognition.face_encodings(img))
-    print(face_embedding.shape)
+    print(face_embedding)
     model = keras.models.load_model(args.model_path)
-    predicton = model.predict_classes(face_embedding)
-    print(labels[predicton[0]])
+    predictons = model.predict_classes(face_embedding)
+    for predicton in predictons:
+        print(labels[predicton])
+    img = cv2.resize(img,(160,160))
     cv2.imshow("Goal",img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
